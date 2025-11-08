a77c7aebdeac986705dbabb8abf421a72a53ad5c
Warley Vieira <warleyjunio2004@gmail.com>
2025-11-08 16:42:30 -0300

    coin-change: retirando comentários desnecessários

--- 02-coin-change/index.js
+++ 02-coin-change/index.js
@@ -1,19 +1,17 @@
 const fs = require("fs");
 
 function coinChange(coins, amount) {
-  const INF = amount + 1; // "infinito" prático
+  const INF = amount + 1; 
   const dp = new Array(amount + 1).fill(INF);
 
-  dp[0] = 0; // 0 moedas pra fazer 0
+  dp[0] = 0; 
 
-  // programação dinâmica: para cada moeda, tenta usá-la
   for (const coin of coins) {
     for (let a = coin; a <= amount; a++) {
       dp[a] = Math.min(dp[a], dp[a - coin] + 1);
     }
   }
 
-  // se não conseguiu chegar exatamente em 'amount'
   return dp[amount] === INF ? -1 : dp[amount];
 }
 
